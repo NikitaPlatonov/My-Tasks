9dce3a1fba42b35ea3b67bd4243ccaa57664669f
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-07-11 10:03:19 +0300

    Добавил в контроллер(--> MainController)  методы отображения и обработки данных на странице(--> createTask.html)

--- src/main/java/ru/myprojects/myTasks/Controllers/MainController.java
+++ src/main/java/ru/myprojects/myTasks/Controllers/MainController.java
@@ -1,13 +1,30 @@
 package ru.myprojects.myTasks.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.myprojects.myTasks.DB.Task;
+import ru.myprojects.myTasks.Repository.RepoDB;
 
 @Controller
 public class MainController {
+    private RepoDB repoDB;
 
     @GetMapping
     public String main(){
         return "mainPage";
+    }
+    @GetMapping("/createTask")
+    public String createTask(){
+        return "createTask";
+    }
+    public String postCreateTask(@RequestParam(name = "taskName") String taskName, @RequestParam(name = "taskDescription") String taskDescription, Model model){
+        Task t = new Task();
+        t.setTaskName(taskName);
+        t.setTaskDescription(taskDescription);
+        repoDB.save(t);
+        model.addAttribute("message", "Задача успешно сохранена");
+        return "createTask";
     }
 }
---
+++ src/main/java/ru/myprojects/myTasks/Repository/RepoDB.java
@@ -0,0 +1,9 @@
+package ru.myprojects.myTasks.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ru.myprojects.myTasks.DB.Task;
+
+@Repository
+public interface RepoDB extends CrudRepository<Task, Long> {
+}
